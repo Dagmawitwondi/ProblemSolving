2c8d8355a3f4f0b904d236deee56f5e2311b039f
Daniel Tilahun <tiledan2015@gmail.com>
2024-11-17 09:39:47 +0300

    remove comments

--- StringLength.java
+++ StringLength.java
@@ -1,9 +1,7 @@
 public class StringLength {
 
-    // Method to calculate the length of a string
     public static int calculateLength(String str) {
         int length = 0;
-        // Loop through each character of the string
         for (char c : str.toCharArray()) {
             length++;
         }
@@ -11,10 +9,7 @@ public class StringLength {
     }
 
     public static void main(String[] args) {
-        // Example string
         String input = "Hello, world!";
-        
-        // Calculate and print the length of the string
         System.out.println("The length of the string is: " + calculateLength(input));
     }
 }
